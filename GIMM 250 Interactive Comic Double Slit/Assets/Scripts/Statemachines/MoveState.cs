a7214e11c20a7efd4bb58ccf40c1ca9df1e40139
psychoaurora <lukebennett@u.boisestate.edu>
2026-04-14 18:49:37 -0600

    Greyboxes and State Machine
    -Greyboxes for hub world, adult school, and courtyard and hotel
    -Set up state machine with three states: idle, move, and jump.

---
+++ GIMM 250 Interactive Comic Double Slit/Assets/Scripts/Statemachines/MoveState.cs
@@ -0,0 +1,51 @@
+using System.Threading;
+using UnityEngine;
+
+public class MoveState : IPlayerState
+{
+    private readonly PlayerStateMachine machine;
+
+    public MoveState(PlayerStateMachine machine)
+    {
+        this.machine = machine;
+    }
+
+    public void Enter()
+    {
+        Debug.Log("Entered Move State");
+        //Maybe start an animation in here or something
+    }
+
+    public void Update()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+        Debug.Log(machine.HorizontalSpeed);
+        Vector2 movement = new Vector2(input * machine.HorizontalSpeed, machine.rb.linearVelocity.y);
+        Debug.Log(movement);
+        machine.rb.linearVelocity = movement;
+
+        if (input < 0)
+        {
+            machine.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (input > 0)
+        {
+            machine.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (Mathf.Abs(input) < .01f)
+        {
+            machine.SwitchState(new IdleState(machine));
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            machine.SwitchState(new JumpState(machine));
+        }
+    }
+
+    public void Exit()
+    {
+        //Optional cleanup
+        //Probably stop playing anim here
+    }
+}
---
+++ GIMM 250 Interactive Comic Double Slit/Assets/Scripts/Statemachines/PlayerStateMachine.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour
+{
+
+    [Header("Movement properties")]
+    [SerializeField] private float horizontalSpeed ;
+    [SerializeField] private float jumpForce;
+
+    [Header("Movement step rate")]
+    [SerializeField] private float movementFps = 12f;
+
+    private IPlayerState currentState;
+
+    [HideInInspector] public Rigidbody2D rb;
+
+    public bool isGrounded;
+    public int jumps = 1;
+    public int maxJumps = 1;
+   
+
+    public float StepInterval => 1f / movementFps;
+
+    void Start()
+    {
+        SwitchState(new IdleState(this));
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState?.Update();
+
+        CheckGround();
+    }
+
+    public void SwitchState(IPlayerState newState)
+    {
+        if (currentState != null)
+        {
+            currentState.Exit();
+        }
+
+        currentState = newState;
+        currentState.Enter();
+    }
+
+    //Accessors
+    public Transform GetTransform() => transform;
+    public float HorizontalSpeed
+    {
+        set
+        {
+            horizontalSpeed = value;
+        }
+        get
+        {
+            return horizontalSpeed;
+        }
+    }
+    public float JumpForce
+    {
+        set
+        {
+            jumpForce = value;
+        }
+        get
+        {
+            return jumpForce;
+        }
+    }
+
+    public void CheckGround()
+    {
+        float radius = .1f; // The radius of the circle cast
+        float distance = 1.1f; // The distance of the circle cast
+        Vector2 origin = transform.position; // The position of the ground check object
+        Vector2 direction = Vector2.down; // The direction of the raycast
+        LayerMask layerMask = LayerMask.GetMask("Ground", "Platform", "Wall"); // The layers the raycast should hit
+
+        /* CircleCast is a 2D sphere cast that checks for colliders in a circular area.
+            RaycastHit2D is a struct that stores information about the raycast hit. 
+            Raycasts are basically invisible rays that are used to detect objects in the scene. */
+        RaycastHit2D hit = Physics2D.CircleCast(origin, radius, direction, distance, layerMask);
+
+        // If the raycast hits a collider, the player is grounded. Otherwise, the player is not grounded.
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        Debug.DrawRay(origin, direction * distance, Color.red);
+    }
+}
+
